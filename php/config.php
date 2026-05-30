bf2d3d00a2b67476dd0ab5406b938d64db9ef1cb
AnaLauraGomez <a23310150@ceti.mx>
2026-05-30 16:39:11 -0600

    Configuracion inicial de GitHub Actions

--- php/config.php
+++ php/config.php
@@ -1,4 +1,17 @@
 <?php
+#$server = "sql309.infinityfree.com"; 
+#$database = "if0_42036749_azuly"; 
+#$user = "if0_42036749"; 
+#$password = "9gt4MnLYunmz4mr"; // ← Si usas XAMPP normalmente va vacío
+
+#$con = mysqli_connect($server, $user, $password, $database);
+
+#if (!$con) {
+#    die("❌ Error al conectar a la base de datos: " . mysqli_connect_error());
+#} else {
+#    //echo "✅ Conexión exitosa";
+#}
+
 $server = "localhost"; 
 $database = "azuly"; 
 $user = "root"; 
